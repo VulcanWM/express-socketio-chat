be00f20bdde3027fa0e9ef324d244a73c8f5ba6e
VulcanWM <logmedha09@gmail.com>
2023-07-13 15:35:55 +0100

    added a views folder

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/chat.ejs');
+  res.render("chat")
 });
 
 const users = {};
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-
     socket.on('login', function(username){
         users[socket.id] = username;
         io.emit("user joined", username)
@@ -23,6 +22,7 @@ io.on('connection', (socket) => {
         io.emit("user left", users[socket.id])
         delete users[socket.id];
     });
+
     socket.on('chat message', (user, msg) => {
         io.emit('chat message', user, msg);
     });
